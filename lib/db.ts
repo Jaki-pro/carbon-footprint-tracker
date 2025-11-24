1b9666f87ee04ae594b68a0acb8f89b56c39deb5
Jakaria Hossain <mdjak8980@gmail.com>
2025-11-24 22:32:54 +0600

    cleaned db config

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,8 @@
-import { Pool } from "pg";
-console.log(process.env.DATABASE_URL);
+import { Pool } from "pg"; 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true, // disable SSL for local database server as local database has no SSL connection
 });
 
 export default pool;
-/*
-import { Client } from "pg";
-
-// Create a single client connection
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: false, // for local development
-});
-client manages a single connection where at a time only one database query is executable.
-Pool manages multiple connection simulatenously
-*/
+ 
