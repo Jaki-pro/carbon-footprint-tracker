667a6eca6d4838e108d21f40f242a26b0ecedd6b
jaki-pro <mdjak8980@gmail.com>
2025-12-09 08:30:22 +0600

    apply security patch-npx fix-react2shell-next

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth';
-import pool from '@/lib/db';
-import { unstable_cache } from 'next/cache';
-import { processActivityData } from '@/utils/processActivityData';
-import { redirect } from 'next/navigation';
-import { Loader } from 'lucide-react';
-import DashboardClient from '@/components/dashboard/DashboardClient';
-import { cacheTag } from 'next/dist/server/use-cache/cache-tag';
+import React from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import pool from "@/lib/db";
+import { unstable_cache } from "next/cache";
+import { processActivityData } from "@/utils/processActivityData";
+import { redirect } from "next/navigation";
+import { Loader } from "lucide-react";
+import DashboardClient from "@/components/dashboard/DashboardClient";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
 
 const getActivitiesByUser = async (userId: number) => {
-
-  'use cache'
-  cacheTag('activities');
+  "use cache";
+  cacheTag("activities");
 
   const sql = `
       SELECT
@@ -30,22 +29,23 @@ const getActivitiesByUser = async (userId: number) => {
       ORDER BY a.activity_date DESC
     `;
   const result = await pool.query(sql, [userId]);
-  return result.rows.map(row => ({
+  return result.rows.map((row) => ({
     ...row,
     activity_date: new Date(row.activity_date).toISOString(),
   }));
-
 };
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user?.email) {
-    redirect('/api/signin');
+    redirect("/api/signin");
   }
 
   // Fetch User ID
-  const userRes = await pool.query('SELECT id FROM "Users" WHERE email = $1', [session.user.email]);
+  const userRes = await pool.query('SELECT id FROM "Users" WHERE email = $1', [
+    session.user.email,
+  ]);
 
   if (userRes.rows.length === 0) {
     return <div className="p-8">User account not found.</div>;
